6b3331e23a1a7415237f4f8e404fdbd8ee65bf73
David Hacker <dmhacker@protonmail.com>
2019-12-01 11:05:02 -0800

    Add covariance test for basic discrete randvars

--- tests/test_basic_discrete_randvars.py
+++ tests/test_basic_discrete_randvars.py
@@ -2,30 +2,52 @@ import pytest
 
 from alea.discrete import DiscreteRandVar
 
+
 class TestDiscrete:
 
     def test_mean(self):
-        def coin_mf(x):
+
+        def pmf(x):
             if x == -1:
                 return 0.5
             else:
                 return 0.5
 
-        coin_ss = {-1, 1}
+        support = {-1, 1}
 
-        X = DiscreteRandVar(coin_ss, coin_mf)
+        X = DiscreteRandVar(support, pmf)
 
         assert (X.mean() == 0)
 
+
     def test_variance(self):
-        def coin_mf(x):
+
+        def pmf(x):
             if x == -1:
                 return 0.5
             else:
                 return 0.5
 
-        coin_ss = {-1, 1}
+        support = {-1, 1}
 
-        X = DiscreteRandVar(coin_ss, coin_mf)
+        X = DiscreteRandVar(support, pmf)
 
         assert (X.variance() == 1)
+
+
+    def test_covariance(self):
+
+        def pmf(x):
+            if x == -1:
+                return 0.5
+            else:
+                return 0.5
+
+        support = {-1, 1}
+
+        X = DiscreteRandVar(support, pmf)
+        Y = DiscreteRandVar(support, pmf)
+
+        # X, Y are independent, so their
+        # covariance must be zero
+        assert (X.covariance(Y) == 0)
